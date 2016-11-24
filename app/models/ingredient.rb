ab7520c60665d16f223b17f1212281767e7f00d3
Jeroen Jagt <jeroenpjagt@gmail.com>
2016-11-24 12:06:20 +0100

    Added validations and associations in models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,16 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+
+  before_destroy :before_destroy_check_for_dose_children
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  private
+
+  def before_destroy_check_for_dose_children
+    unless doses.count.zero?
+      raise "Can't destroy ingredient because it has dose children"
+    end
+  end
 end
